ff2a3f01e702a1709f2bc311d72f34a10207596a
unknown <prabeshpathak50@gmail.com>
2022-01-02 15:54:10 +0545

    server changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const app = express();
+const mongooose = require("mongoose");
 
+mongooose.connect("mongodb://localhost/restful_blog_app");
+const PORT = process.env.PORT || 8000;
+
+const app = express();
 app.route("/").get((req, res) => {
   return res.json({ message: "hi" });
 });
 
-app.listen(5000, () => console.log("server started"));
+app.listen(PORT, () => console.log(`server started on ${PORT}`));

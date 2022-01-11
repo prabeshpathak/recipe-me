c67d8a1731a6793c857f03c5f0684ebb9aaa71a1
unknown <prabeshpathak50@gmail.com>
2022-01-11 13:45:53 +0545

    making function and classes for all the required functions

---
+++ lib/utils/AuthProvider.dart
@@ -0,0 +1,299 @@
+// AuthProvider
+// CODE AND INSPIRATION FROM
+// https://medium.com/@afegbua/flutter-thursday-13-building-a-user-registration-and-login-process-with-provider-and-external-api-1bb87811fd1d
+// THANK YOU SHUAIB AFEGBUA
+
+// ignore_for_file: invalid_return_type_for_catch_error
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:recipe_app_flutter/models/User.dart';
+import 'package:recipe_app_flutter/utils/API.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app_flutter/utils/UserPreference.dart';
+
+enum Status {
+  // _verificationStatus
+  NotVerified,
+  Verifying,
+  Verified,
+
+  // _loggedInStatus
+  LoggedIn,
+  Authenticating,
+  LoggedOut,
+  NotLoggedIn,
+
+  // _registeredStatus
+  Registering,
+  Registered,
+  NotRegistered
+}
+
+// Helper function to return the decoded base 64 string.
+String _decodeBase64(String str) {
+  String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw Exception('Illegal base64url string!"');
+  }
+
+  return utf8.decode(base64Url.decode(output));
+}
+
+// Returns the middle section of a JWT containing mostly the user data
+Map<String, dynamic> parseJwt(String token) {
+  final parts = token.split('.');
+  final payload = _decodeBase64(parts[1]);
+  final payloadMap = json.decode(payload);
+  return payloadMap;
+}
+
+class AuthProvider with ChangeNotifier {
+  Status _loggedInStatus = Status.NotLoggedIn;
+  Status _registeredStatus = Status.NotRegistered;
+  Status _verificationStatus = Status.NotVerified;
+  Status get loggedInStatus => _loggedInStatus;
+  Status get registeredStatus => _registeredStatus;
+  Status get verificationStatus => _verificationStatus;
+  set loggedInStatus(value) => _loggedInStatus = value;
+  set registeredStatus(value) => _registeredStatus = value;
+  set verificationStatus(value) => _verificationStatus = value;
+
+  // Saves login data given a response's token
+  User saveLogin(Map<String, dynamic> responseData) {
+    var userData = parseJwt(responseData['token']);
+    userData['token'] = responseData['token'];
+    User authUser = User.fromMap(userData);
+    UserPreference().saveUser(authUser);
+    return authUser;
+  }
+
+  // Helper function to perform login after an API call. Mostly checks response
+  // codes, sets AuthProvider state, and such.
+  Map<String, dynamic> authLogin(Map<String, dynamic> responseData) {
+    var result;
+
+    // If there was no issue with registering or verifying...
+    if (responseData['code'] == 200 && responseData['error'] == null) {
+      User user = saveLogin(responseData);
+
+      if (user.verified) {
+        // User is verified
+        _loggedInStatus = Status.LoggedIn;
+        _verificationStatus = Status.Verified;
+        _registeredStatus = Status.Registered;
+        result = {
+          'status': true,
+          'message': 'Successfully logged in',
+          'user': user
+        };
+      } else {
+        // User is not verified
+        _loggedInStatus = Status.NotLoggedIn;
+        _verificationStatus = Status.Verifying;
+        _registeredStatus = Status.Registered;
+
+        result = {
+          'status': false,
+          'message': 'Please verify your account',
+          'user': user
+        };
+      }
+    } else {
+      // There was likely some error with the API call if we get here
+      _loggedInStatus = Status.NotLoggedIn;
+      result = {
+        'status': false,
+        'code': responseData['code'],
+        'message': responseData['error']
+      };
+    }
+
+    return result;
+  }
+
+  // Call to perform entire google login & sign up
+  Future<Map<String, dynamic>> googleLogin() async {
+    var result;
+    GoogleSignInAccount? account;
+    GoogleSignInAuthentication googleSignInAuthentication;
+    GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+    // Attempt fetching a google account
+    try {
+      account = await _googleSignIn.signIn();
+      // Grab the authentication details and update state of the provider
+      googleSignInAuthentication = await account!.authentication;
+      _loggedInStatus = Status.Authenticating;
+      notifyListeners();
+    } catch (e, stacktrace) {
+      // Failed to select an account
+      _loggedInStatus = Status.NotLoggedIn;
+      notifyListeners();
+      return {
+        'status': false,
+        'message': 'Failed to complete Google sign in: ${stacktrace.toString()}'
+      };
+    }
+
+    // Handle responses
+    try {
+      // Call to the API to sign up and/or login.
+      final Map<String, dynamic> responseData = await API()
+          .googleVerification(googleSignInAuthentication.idToken.toString());
+      result = authLogin(responseData);
+    } catch (on, stacktrace) {
+      result = {'status': false, 'message': stacktrace.toString()};
+    }
+
+    return result;
+  }
+
+  // API call to login a user and save login info.
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    var result;
+    print(email + password + "asdfdsffa");
+    _loggedInStatus = Status.Authenticating;
+    notifyListeners();
+
+    try {
+      final Map<String, dynamic> responseData =
+          await API().doLogin(email, password);
+      result = authLogin(responseData);
+    } catch (on, stacktrace) {
+      print(stacktrace.toString());
+      result = {'status': false, 'message': stacktrace.toString()};
+    }
+
+    notifyListeners();
+    return result;
+  }
+
+  // API call to login a user and save login info.
+  Future<Map<String, dynamic>> resetPasswordLogin(
+      String email, String verification, String password) async {
+    var result;
+
+    _verificationStatus = Status.Verifying;
+    notifyListeners();
+
+    _loggedInStatus = Status.NotLoggedIn;
+    _verificationStatus = Status.NotVerified;
+    _registeredStatus = Status.NotRegistered;
+
+    notifyListeners();
+    return result;
+  }
+
+  // API call to signup a user and save login info.
+  Future<Map<String, dynamic>> signup(
+      String name, String email, String password) async {
+    var result;
+
+    _verificationStatus = Status.NotVerified;
+    _registeredStatus = Status.Registering;
+    _loggedInStatus = Status.NotLoggedIn;
+    notifyListeners();
+    print('here');
+    try {
+      print('here-success');
+
+      final Map<String, dynamic> responseData =
+          await API().doSignup(name, email, password);
+      print(responseData);
+
+      switch (responseData['code']) {
+        case 200:
+          _verificationStatus = Status.NotVerified;
+          _registeredStatus = Status.Registered;
+          _loggedInStatus = Status.NotLoggedIn;
+          result = {
+            'status': true,
+            'message': 'Successfully registered, please verify your account',
+            'user': saveLogin(responseData)
+          };
+          break;
+        default:
+          _verificationStatus = Status.NotVerified;
+          _registeredStatus = Status.NotRegistered;
+          _loggedInStatus = Status.NotLoggedIn;
+          result = {'status': false, 'message': responseData['error']};
+      }
+    } catch (on, stacktrace) {
+      print('here-atuh');
+      _verificationStatus = Status.NotVerified;
+      _registeredStatus = Status.NotRegistered;
+      _loggedInStatus = Status.NotLoggedIn;
+      result = {'status': false, 'message': stacktrace.toString()};
+    }
+
+    notifyListeners();
+    return result;
+  }
+
+  Future<Map<String, dynamic>> verify(String code, String token) async {
+    var result;
+
+    _verificationStatus = Status.Verifying;
+    notifyListeners();
+
+    try {
+      final Map<String, dynamic> responseData =
+          await API().checkVerification(token, code);
+
+      switch (responseData['code']) {
+        case 200: // Successful verification
+          UserPreference().verifyUser();
+          _loggedInStatus = Status.LoggedIn;
+          _verificationStatus = Status.Verified;
+          _registeredStatus = Status.Registered;
+          result = {
+            'status': true,
+            'message': 'Sucessfully verified and logged in'
+          };
+          break;
+        case 400: // Code timed out
+          _loggedInStatus = Status.NotLoggedIn;
+          _verificationStatus = Status.NotVerified;
+          _registeredStatus = Status.Registered;
+          result = {
+            'status': false,
+            'message': 'Failed to verify: entered an expired code'
+          };
+          break;
+        case 422: // Invalid code responseData
+          result = {
+            'status': false,
+            'message': 'Failed to verify: invalid code'
+          };
+          break;
+        default:
+          result = {'status': false, 'message': responseData.toString()};
+      }
+    } catch (on, stacktrace) {
+      result = {'status': false, 'message': stacktrace.toString()};
+    }
+
+    notifyListeners();
+    return result;
+  }
+
+  void logout() {
+    UserPreference().removeUser();
+    _loggedInStatus = Status.NotLoggedIn;
+    _registeredStatus = Status.NotRegistered;
+    _verificationStatus = Status.NotVerified;
+    notifyListeners();
+  }
+}

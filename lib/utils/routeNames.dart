9bb0c21cf3ea4792b313caf6983d27c5be711a57
unknown <prabeshpathak50@gmail.com>
2022-01-12 13:47:27 +0545

    small files for routes

---
+++ lib/utils/routeNames.dart
@@ -0,0 +1,8 @@
+class RouteName {
+  static const String LANDING = 'landing';
+  static const String SIGNUP = 'signup';
+  static const String LOGIN = 'login';
+  static const String FORGOTPASSWORD = 'forgotpassword';
+  static const String VERIFICATION = 'verification';
+  static const String HOME = 'home';
+}

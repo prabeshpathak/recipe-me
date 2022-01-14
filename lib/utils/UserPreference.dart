5ccec535a2be006e7718ea05e643e864c5782d1a
unknown <prabeshpathak50@gmail.com>
2022-01-14 13:49:54 +0545

    getting all the user functions in one file

---
+++ lib/utils/UserPreference.dart
@@ -0,0 +1,44 @@
+import 'package:recipe_app_flutter/models/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+
+class UserPreference {
+  Future<bool> verifyUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setBool('verified', true);
+  }
+
+  Future<bool> saveUser(User user) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userId', user.userId);
+    prefs.setString('name', user.name);
+    prefs.setBool('verified', user.verified);
+
+    return prefs.setString('token', user.token);
+  }
+
+  Future<User> getUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String userId = prefs.getString('userId') ?? 'null';
+    String name = prefs.getString('name') ?? 'null';
+    String token = prefs.getString('token') ?? 'null';
+    bool verified = prefs.getBool('verified') ?? false;
+
+    return User(userId: userId, name: name, token: token, verified: verified);
+  }
+
+  void removeUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('userId');
+    prefs.remove('name');
+    prefs.remove('token');
+    prefs.remove('verified');
+  }
+
+  Future<String> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token') ?? 'null';
+    return token;
+  }
+}

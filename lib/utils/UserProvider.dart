e662f4024b1f464dd0c1c1df9ae0b70e193bd5c9
unknown <prabeshpathak50@gmail.com>
2022-01-14 13:50:40 +0545

    initizlization of the user

---
+++ lib/utils/UserProvider.dart
@@ -0,0 +1,16 @@
+import 'package:recipe_app_flutter/models/User.dart';
+import 'package:flutter/material.dart';
+
+class UserProvider with ChangeNotifier {
+  User _user = new User(userId: 'null', name: 'null', token: 'null');
+  User get user => _user;
+
+  void initializeUser(User user) {
+    _user = user;
+  }
+
+  void setUser(User user) {
+    _user = user;
+    notifyListeners();
+  }
+}

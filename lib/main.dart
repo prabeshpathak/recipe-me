c3d04b27fed9a80456d180b2ea3570c8bab5cad8
unknown <prabeshpathak50@gmail.com>
2022-01-15 13:35:14 +0545

    added signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app_flutter/pages/SignUp.dart';
 import 'package:recipe_app_flutter/pages/WelcomePage.dart';
 import "package:google_fonts/google_fonts.dart";
 
---
+++ lib/pages/SignUp.dart
@@ -0,0 +1,172 @@
+import "package:flutter/material.dart";
+
+class SignUpPage extends StatefulWidget {
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool vis = true;
+  final GlobalKey<FormState> _globalkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // height: MediaQuery.of(context).size.height,
+        // width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.blue.shade300],
+            begin: const FractionalOffset(0.0, 1.0),
+            end: const FractionalOffset(0.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.repeated,
+          ),
+        ),
+        child: Form(
+          key: _globalkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign up with email",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              usernameTextField(),
+              emailTextField(),
+              passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  if (_globalkey.currentState!.validate()) {
+                    // we will send the data to rest server
+                    print("Validated");
+                  }
+                },
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff00A86B),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget usernameTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Username"),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) return "Username can't be empty";
+              // username unique is not
+              return null;
+            },
+            decoration: InputDecoration(
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget emailTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Email"),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email is Invalid";
+              return null;
+            },
+            decoration: InputDecoration(
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget passwordTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Password"),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) return "Password can't be empty";
+              if (value.length < 8) return "Password lenght must have >=8";
+              return null;
+            },
+            obscureText: vis,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(vis ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    vis = !vis;
+                  });
+                },
+              ),
+              helperText: "Password length should be atleast 8",
+              helperStyle: TextStyle(
+                fontSize: 14,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

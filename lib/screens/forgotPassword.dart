6e82b7f9cb38937a4d751ef4066f6aa22078c373
unknown <prabeshpathak50@gmail.com>
2022-01-18 14:06:00 +0545

    Screen: Adding forgotpassword and verification screen

---
+++ lib/screens/forgotPassword.dart
@@ -0,0 +1,264 @@
+import 'package:recipe_app_flutter/utils/API.dart';
+import 'package:recipe_app_flutter/utils/RouteNames.dart';
+import 'package:recipe_app_flutter/widgets/InputBox.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app_flutter/utils/StringValidator.dart';
+
+enum Step { email, verification }
+
+// This is the state of the ForgotPassword screen. This changes depending on
+// the selectedStep. The function setState() forces an update of the widget.
+class ForgotPasswordState extends State<ForgotPassword> {
+  // Form key is important for implementation of the InputBox class and reading the
+  // form's fields. It essentially attaches to each field.
+  final _formKeyEmail = GlobalKey<FormState>();
+  final _formKeyVerification = GlobalKey<FormState>();
+
+  // TextEditingControllers allow for simple getters from the fields.
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _verification = TextEditingController();
+  final TextEditingController _pass = TextEditingController();
+  final TextEditingController _confirmPass = TextEditingController();
+
+  // The current state of the widget.
+  Step selectedStep = Step.email;
+
+  String email = '';
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      keyboardType: TextInputType.emailAddress,
+      controller: _email,
+      textInputAction: TextInputAction.done,
+      decoration: const InputDecoration(
+        hintText: 'Email',
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.only(left: 15.0),
+      ),
+      validator: (value) {
+        var empty = value?.isEmpty ?? true;
+        if (empty) {
+          return 'Please enter your email';
+        } else if (!value.isValidEmail()) {
+          return 'Please enter a valid email';
+        }
+        return null;
+      },
+    );
+  }
+
+  // Function to build and return a password field text box.
+  Widget _buildPasswordField() {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      textInputAction: TextInputAction.next,
+      controller: _pass,
+      obscureText: true,
+      enableSuggestions: false,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.only(left: 15.0),
+        border: OutlineInputBorder(),
+        hintText: 'Password',
+      ),
+      validator: (value) {
+        var empty = value?.isEmpty ?? true;
+        if (empty) {
+          return 'Please enter your password';
+        } else if (!value.isValidPassword()) {
+          return 'Please enter a password with at least eight characters';
+        }
+        return null;
+      },
+    );
+  }
+
+  // Function to build and return a confirm password field text box.
+  Widget _buildConfirmPasswordField() {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      textInputAction: TextInputAction.done,
+      controller: _confirmPass,
+      obscureText: true,
+      enableSuggestions: false,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.only(left: 15.0),
+        border: OutlineInputBorder(),
+        hintText: 'Confirm password',
+        // labelText: 'Name *',
+      ),
+      // The validator receives the text that the user has entered.
+      validator: (value) {
+        var empty = value?.isEmpty ?? true;
+        if (empty) {
+          return 'Please confirm your password';
+        } else if (value != _pass.text) {
+          return 'Passwords do not match';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildVerificationField() {
+    return TextFormField(
+      controller: _verification,
+      textInputAction: TextInputAction.done,
+      decoration: const InputDecoration(
+        hintText: 'Verification',
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.only(left: 15.0),
+      ),
+      validator: (value) {
+        var empty = value?.isEmpty ?? true;
+        if (empty) {
+          return 'Please your verification code';
+        }
+        return null;
+      },
+    );
+  }
+
+  void verifyPasswordResult(Map<String, dynamic> responseData) {
+    var result;
+    try {
+      switch (responseData['code']) {
+        case 200:
+          result = {'status': true, 'message': 'Password Reset Successful'};
+          break;
+        case 404:
+          result = {'status': false, 'message': 'Email does not exist'};
+          break;
+        case 410:
+          result = {'status': false, 'message': 'Verification code timed out'};
+          break;
+        case 500:
+          result = {'status': false, 'message': 'Server error'};
+          break;
+        case 422:
+          result = {
+            'status': false,
+            'message': 'No valid reset can be made for account'
+          };
+          break;
+        default:
+          result = {'status': false, 'message': responseData.toString()};
+      }
+    } catch (on, stacktrace) {
+      result = {'status': false, 'message': stacktrace.toString()};
+    }
+
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(result['message'])));
+    if (result['status'])
+      Navigator.pushNamedAndRemoveUntil(context, RouteName.LOGIN, (_) => false);
+  }
+
+  // Important function called to handle submits of the pages. This function
+  // will call the setState() on the widget depending on the current state.
+  void handleSubmit(context) {
+    switch (selectedStep) {
+      // If we are currently on the email state...
+      case Step.email:
+        var validated = _formKeyEmail.currentState?.validate() ?? false;
+        email = _email.text.trim();
+        if (validated && email != '') {
+          // ignore: return_of_invalid_type_from_catch_error
+          API().sendPasswordReset(email).catchError((error) => {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(error)))
+              });
+          setState(() {
+            selectedStep = Step.verification;
+          });
+        }
+        break;
+      // If we are currently on the verification state...
+      case Step.verification:
+        var validated = _formKeyVerification.currentState?.validate() ?? false;
+
+        if (validated) {
+          API()
+              .verifyPasswordReset(email, _verification.text.trim(), _pass.text)
+              .then((value) => {verifyPasswordResult(value)});
+        }
+        break;
+    }
+  }
+
+  Widget _buildSubmit(context) {
+    return ElevatedButton(
+      onPressed: () {
+        handleSubmit(context);
+      },
+      child: Text('Next'),
+    );
+  }
+
+  Widget _buildInputBox(context) {
+    switch (selectedStep) {
+      case Step.email:
+        return InputBox(
+            'Find your Recipe Me account',
+            <Widget>[
+              _buildEmailField(),
+              SizedBox(height: 10),
+            ],
+            _formKeyEmail,
+            _buildSubmit(context));
+      case Step.verification:
+        return InputBox(
+            'Enter verification',
+            <Widget>[
+              _buildVerificationField(),
+              SizedBox(height: 10),
+              _buildPasswordField(),
+              SizedBox(height: 10),
+              _buildConfirmPasswordField(),
+            ],
+            _formKeyVerification,
+            _buildSubmit(context));
+    }
+  }
+
+  Widget _buildCenter(context) {
+    return Center(
+        child: Container(
+      width: 370,
+      padding: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: _buildInputBox(context),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/BackgroundBlurred.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[_buildCenter(context)],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// This is the main function that returns the widget itself. It depends on the
+// ForgotPasswordState class.
+class ForgotPassword extends StatefulWidget {
+  State<StatefulWidget> createState() => ForgotPasswordState();
+}
---
+++ lib/screens/verification.dart
@@ -0,0 +1,172 @@
+import 'package:recipe_app_flutter/models/User.dart';
+import 'package:recipe_app_flutter/screens/landing.dart';
+import 'package:recipe_app_flutter/utils/API.dart';
+import 'package:recipe_app_flutter/utils/AuthProvider.dart';
+import 'package:recipe_app_flutter/utils/RouteNames.dart';
+import 'package:recipe_app_flutter/utils/UserProvider.dart';
+import 'package:recipe_app_flutter/widgets/InputBox.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Verification extends StatefulWidget {
+  @override
+  VerificationState createState() => new VerificationState();
+}
+
+class VerificationState extends State<Verification> {
+  final _formKey = GlobalKey<FormState>();
+  final _verify = TextEditingController();
+
+  // Function to build and return a login field text box.
+  Widget _buildVerificationField() {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.disabled,
+      keyboardType: TextInputType.number,
+      // Be sure to pass in the text controller to fields to grab their text.
+      controller: _verify,
+      textInputAction: TextInputAction.done,
+      decoration: const InputDecoration(
+        hintText: 'Verification code',
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.only(left: 15.0),
+      ),
+      validator: (value) {
+        if (value?.isEmpty ?? true)
+          return 'Please enter your verification code';
+        return null;
+      },
+    );
+  }
+
+  Future<Map<String, dynamic>> handleRequestCode(context) async {
+    var result;
+    String token = Provider.of<UserProvider>(context, listen: false).user.token;
+
+    final Map<String, dynamic> responseData =
+        await API().requestVerification(token);
+
+    try {
+      switch (responseData['code']) {
+        case 200:
+          result = {
+            'status': true,
+            'code': responseData['code'],
+            'message': 'Sent a new verification email'
+          };
+          break;
+        default:
+          result = {
+            'status': false,
+            'code': responseData['code'],
+            'message': 'Failed to send a new verification email'
+          };
+      }
+    } catch (on, stacktrace) {
+      result = {
+        'status': false,
+        'message':
+            'Failed to send a new verification email: ${stacktrace.toString()}'
+      };
+    }
+
+    return result;
+  }
+
+  Widget _buildRequestCode(context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.all(2),
+        shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+      ),
+      onPressed: () {
+        handleRequestCode(context).then(
+          (value) => ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(value['message']))),
+        );
+      },
+      child: Text('Request a verification code'),
+    );
+  }
+
+  void handleSubmit(context) {
+    User user = Provider.of<UserProvider>(context, listen: false).user;
+    AuthProvider auth = Provider.of<AuthProvider>(context, listen: false);
+
+    auth
+        .verify(_verify.text, user.token)
+        .then((value) => {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(value['message']))),
+              if (value['status'] == true)
+                {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, RouteName.HOME, (_) => false)
+                }
+            })
+        // ignore: return_of_invalid_type_from_catch_error
+        .catchError((error) => {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(error.toString())))
+            });
+  }
+
+  // Function to build and return a form submit button. This is critical to the
+  // implementation of the InputBox class.
+  Widget _buildSubmit(context) {
+    return ElevatedButton(
+      onPressed: () {
+        var validated = _formKey.currentState?.validate() ?? false;
+        if (validated) handleSubmit(context);
+      },
+      child: Text('Verify'),
+    );
+  }
+
+  // Function called to build the widget in the center.
+  Widget _buildCenter(context) {
+    return Center(
+      child: Container(
+          width: 370,
+          padding: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: InputBox(
+              'Verify your account',
+              <Widget>[
+                _buildVerificationField(),
+                _buildRequestCode(context),
+                // request a new code button
+              ],
+              _formKey,
+              _buildSubmit(context))),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider auth = Provider.of<AuthProvider>(context);
+
+    return auth.verificationStatus != Status.Verified
+        ? SafeArea(
+            child: Scaffold(
+            body: Container(
+              // Background image. I'm a bit annoyed how each screen reloads it.
+              // Bounty for whoever can fix that
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/BackgroundBlurred.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[_buildCenter(context)],
+              ),
+            ),
+          ))
+        : Landing();
+  }
+}

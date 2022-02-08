2cf8ed8c0d6aafe42746dd3872abc6e3e483ef92
unknown <prabeshpathak50@gmail.com>
2022-02-08 14:33:32 +0545

    Widgets: Complete Likebutton

---
+++ lib/widgets/LikeButton.dart
@@ -0,0 +1,64 @@
+import 'package:recipe_app_flutter/models/Recipe.dart';
+import 'package:recipe_app_flutter/utils/API.dart';
+import 'package:recipe_app_flutter/utils/UserProvider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+import 'package:provider/provider.dart';
+
+class RecipeLikeButton extends StatefulWidget {
+  final Recipe recipe;
+  RecipeLikeButton({required this.recipe});
+
+  @override
+  RecipeLikeButtonState createState() => RecipeLikeButtonState();
+}
+
+class RecipeLikeButtonState extends State<RecipeLikeButton> {
+  late int likeCount = 0;
+  late String token;
+  late bool isLiked;
+  bool liking = false;
+
+  void initState() {
+    super.initState();
+    likeCount = widget.recipe.favorites;
+    isLiked = widget.recipe.isLiked;
+  }
+
+  Future<bool> _toggleLike(String token, bool value) async {
+    Map<String, dynamic> response =
+        await API().doLikeRecipe(token, widget.recipe.recipeId);
+    setState(() {
+      if (response['code'] == 200) {
+        likeCount = response['numFavorites'];
+        isLiked = !value;
+      }
+      liking = false;
+    });
+
+    return Future.value(isLiked);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String token = Provider.of<UserProvider>(context).user.token;
+    return LikeButton(
+        size: 30,
+        likeCountAnimationDuration: Duration(milliseconds: 200),
+        onTap: (value) {
+          if (liking)
+            return Future.value(value);
+          else {
+            liking = true;
+            return _toggleLike(token, value);
+          }
+        },
+        isLiked: isLiked,
+        likeCount: likeCount,
+        countBuilder: (count, bool isLiked, String text) {
+          var color = isLiked ? Colors.pink : Colors.black;
+          return Text(text, style: TextStyle(color: color));
+        });
+  }
+}
